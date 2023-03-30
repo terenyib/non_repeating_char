081e7123e4fbdd1b942ff8298cce586e040b1efd
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-30 20:35:59 +0200

    refactor

--- src/non_repeating_char.js
+++ src/non_repeating_char.js
@@ -1,8 +1,8 @@
 function nonRepeatingChar(text) {
   if (text.length == 1) {
     return text;
-  } else if (text == "ab"){
-    return "a";
+  } else if (text[0] != text[1]){
+    return text[0];
   } else {
     return "";
   }
