70761f56d7c30b9e5ca2f176cbe5bba8ada87776
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-30 21:23:14 +0200

    UAT-4.1 green

--- src/non_repeating_char.js
+++ src/non_repeating_char.js
@@ -3,7 +3,7 @@ function nonRepeatingChar(text) {
   for(let i = 0; i < text.length; i++) {
     let currentChar = text[i];
     let text2 = text.slice(0, i).concat(text.slice(i + 1))
-    if (!text2.includes(currentChar) && result == "") {
+    if (result == "" && !text2.toUpperCase().includes(currentChar) && !text2.toLowerCase().includes(currentChar)) {
       result += currentChar;
     }
   }
