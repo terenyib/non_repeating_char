c764fb82c3d86eaef9e6fb7a3117449e3f6b4539
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-30 20:40:08 +0200

    UAT-3.1 red

--- src/non_repeating_char.test.js
+++ src/non_repeating_char.test.js
@@ -9,10 +9,15 @@ describe('US-1', () => {
   });
 });
 describe('US-2', () => {
-  it('Given text "ab" When I check first non-repeating Then the result is "ab"', () => {
+  it('Given text "ab" When I check first non-repeating Then the result is "a"', () => {
     expect(nonRepeatingChar("ab")).toBe("a");
   });
   it('Given text "aa" When I check first non-repeating Then the result is ""', () => {
     expect(nonRepeatingChar("aa")).toBe("");
+  });
+});
+describe('US-3', () => {
+  it('Given text "aba" When I check first non-repeating Then the result is "b"', () => {
+    expect(nonRepeatingChar("aba")).toBe("b");
   });
 });
