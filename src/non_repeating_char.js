2f1e9f86081fcc550454cc6ec7a50b0f23d73322
Balázs Terényi <balazs.terenyi@gmail.com>
2023-03-30 21:12:51 +0200

    refactor

--- src/non_repeating_char.js
+++ src/non_repeating_char.js
@@ -1,17 +1,13 @@
 function nonRepeatingChar(text) {
-  if (text.length == 1) {
-    return text;
-  } else if (text.length == 2 && text[0] != text[1]){
-    return text[0];
-  } else if (text === "aba") {
-    return "b";
-  } else if (text === "aac") {
-    return "c";
-  } else if (text === "abc") {
-    return "a";
-  } else {
-    return "";
+  let result = "";
+  for(let i = 0; i < text.length; i++) {
+    let currentChar = text[i];
+    let text2 = text.slice(0, i).concat(text.slice(i + 1))
+    if (!text2.includes(currentChar) && result == "") {
+      result += currentChar;
+    }
   }
+  return result;
 }
 
 module.exports = nonRepeatingChar;
